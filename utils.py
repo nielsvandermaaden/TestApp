2c9b489d0df84e9960b9d0f807ff6336979e001d
nielsvandermaaden <140918565+nielsvandermaaden@users.noreply.github.com>
2023-07-31 23:44:17 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,20 +1,15 @@
 from pydantic import BaseModel
 
-class DuikerTool(BaseModel):
+class Test_Tool(BaseModel):
   nr_1: float = 0
   nr_2: float = 0
-
-
 
   # First test function
 
   def add_numbers(self):
 
     nr_1 = self.nr_1
-
     nr_2 = self.nr_2
-
-    
 
     new_nr = nr_1 + nr_2
 
