8f873980ddfdcbd163e4d6220d88464f4cbe600d
nielsvandermaaden <140918565+nielsvandermaaden@users.noreply.github.com>
2023-07-31 23:47:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st 
-from utils import DuikerTool
+from utils import Test_Tool as tt
 
 st.header('App runs from utils.py!!')
 
 nr_1 = st.number_input('select nr 1')
 nr_2 = st.number_input('select nr 2')
 
-duiker = DuikerTool(nr_1=nr_1, nr_2=nr_2)
+duiker = tt(nr_1=nr_1, nr_2=nr_2)
 new_nr = duiker.add_numbers()
 
 st.header(f"new nummero = {new_nr}")
